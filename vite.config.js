eb0d60b9dabfa230c15fe22b3649cbbc2a634cb2
Thi <thiago.andradecontato@hotmail.com>
2024-01-25 01:29:01 +0100

    agora vai, mudamos as variaveis locais, bielo falou umas paradas de compile time, nao entendi bem

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { config } from 'dotenv';
+config(); // Isso substitui require('dotenv').config();
+
+console.log('bielom o proxy auth no vite config é:',process.env.PROXY_AUTH)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       // As requisições para `/auth/*` serão encaminhadas para o servidor Express na porta 3000
-      '/auth': 'http://localhost:3000'
+      '/auth': process.env.PROXY_AUTH
     }
   }
 });
